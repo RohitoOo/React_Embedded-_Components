3e966415845f1f89c85b762f3872f96b27edcf64
RohitoOo <bhambhani.ba@gmail.com>
2018-05-15 17:32:56 +0530

    React Components

---
+++ data.js
@@ -0,0 +1,61 @@
+let users = [
+    {
+        name: 'Akshay ',
+        age: 88,
+        followers: ['Rohit', 'Rohan']
+    },
+
+    {
+        name: 'Rohit',
+        age: 21,
+        followers: ['Akshay']
+    },
+
+    {
+        name: 'Rohan',
+        age: 18,
+        followers: ['Akshay', 'Rohit']
+    },
+
+    {
+        name: 'Nitin',
+        age: 22,
+        followers: ['Akshay', 'Rohit', 'Rohan']
+    },
+
+    {
+        name: 'Babul',
+        age: 26,
+        followers: ['Babul']
+    },
+
+    {
+        name: 'Srikanth',
+        age: 31,
+        followers: ['Akshay']
+    },
+
+    {
+        name: 'Karthik',
+        age: 12,
+        followers: ['Nitin', 'Rohit']
+    },
+
+    {
+        name: 'Prashanth',
+        age: 22,
+        followers: ['Akshay', 'Nitin', 'Babul', 'Karthik']
+    },
+
+    {
+        name: 'Shoeb',
+        age: 25,
+        followers: ['Rohan']
+    },
+
+    {
+        name: 'Aravind',
+        age: 30,
+        followers: ['Shoeb', 'Nitin', 'Akshay']
+    }
+];
